afcacc008267ec6823aded8bb50b181160ea37ba
Bojan <babic.bojan62@gmail.com>
2024-08-31 02:55:27 +0200

    Skill icons fix

--- src/components/HeroSection/HeroAnimation.js
+++ src/components/HeroSection/HeroAnimation.js
@@ -7,12 +7,23 @@ const ParticleBackground = ({ theme }) => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
         let particles = [];
-        const numParticles = 150;
+        const numParticles = 100;
+        let animationFrameId;
+        let mouseX = canvas.width / 2;
+        let mouseY = canvas.height / 2;
 
         const resizeCanvas = () => {
             canvas.width = window.innerWidth;
             canvas.height = window.innerHeight;
+            createParticles();
         };
+
+        const debouncedResize = () => {
+            clearTimeout(resizeTimeout);
+            resizeTimeout = setTimeout(resizeCanvas, 100);
+        };
+
+        let resizeTimeout;
 
         const createParticles = () => {
             particles = [];
@@ -20,8 +31,9 @@ const ParticleBackground = ({ theme }) => {
                 particles.push({
                     x: Math.random() * canvas.width,
                     y: Math.random() * canvas.height,
+                    baseX: Math.random() * canvas.width,
+                    baseY: Math.random() * canvas.height,
                     size: Math.random() * 2 + 1,
-                    baseSize: Math.random() * 2 + 1,
                     speedX: Math.random() * 1.5 - 0.75,
                     speedY: Math.random() * 1.5 - 0.75,
                     opacity: Math.random() * 0.5 + 0.3
@@ -29,63 +41,60 @@ const ParticleBackground = ({ theme }) => {
             }
         };
 
-        const drawParticles = (mouseX, mouseY) => {
+        const drawParticles = () => {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
 
             const particleColor = theme === 'light' ? 'rgba(0, 0, 0, 0.5)' : 'rgba(255, 255, 255, 0.5)';
             ctx.fillStyle = particleColor;
-            ctx.strokeStyle = particleColor;
 
             particles.forEach(particle => {
-                const dx = particle.x - mouseX;
-                const dy = particle.y - mouseY;
+                // Calculate distance from the mouse and move particles slightly towards the cursor
+                const dx = mouseX - particle.x;
+                const dy = mouseY - particle.y;
                 const distance = Math.sqrt(dx * dx + dy * dy);
-                const maxDistance = 100;
-                const scale = 1 - Math.min(distance / maxDistance, 1);
-                particle.size = particle.baseSize + scale * 3;
+                const moveDistance = Math.max(100 - distance, 0) / 20; // Adjust movement intensity
+
+                particle.x += dx * moveDistance * 0.01;
+                particle.y += dy * moveDistance * 0.01;
+
+                particle.x += particle.speedX;
+                particle.y += particle.speedY;
+
+                // Wrap around the canvas edges
+                if (particle.x > canvas.width) particle.x = 0;
+                if (particle.x < 0) particle.x = canvas.width;
+                if (particle.y > canvas.height) particle.y = 0;
+                if (particle.y < 0) particle.y = canvas.height;
 
                 ctx.globalAlpha = particle.opacity;
                 ctx.beginPath();
                 ctx.arc(particle.x, particle.y, particle.size, 0, Math.PI * 2);
                 ctx.fill();
-                ctx.stroke();
-
-                particle.x += particle.speedX;
-                particle.y += particle.speedY;
-
-                if (particle.x > canvas.width || particle.x < 0) {
-                    particle.speedX *= -1;
-                }
-                if (particle.y > canvas.height || particle.y < 0) {
-                    particle.speedY *= -1;
-                }
             });
 
             ctx.globalAlpha = 1.0;
         };
 
-        const animate = (mouseX, mouseY) => {
-            drawParticles(mouseX, mouseY);
-            requestAnimationFrame(() => animate(mouseX, mouseY));
+        const animate = () => {
+            drawParticles();
+            animationFrameId = requestAnimationFrame(animate);
         };
-
-        let mouseX = canvas.width / 2;
-        let mouseY = canvas.height / 2;
 
         const handleMouseMove = (event) => {
             mouseX = event.clientX;
             mouseY = event.clientY;
         };
 
-        window.addEventListener('resize', resizeCanvas);
+        window.addEventListener('resize', debouncedResize);
         canvas.addEventListener('mousemove', handleMouseMove);
         resizeCanvas();
         createParticles();
-        animate(mouseX, mouseY);
+        animate();
 
         return () => {
-            window.removeEventListener('resize', resizeCanvas);
+            window.removeEventListener('resize', debouncedResize);
             canvas.removeEventListener('mousemove', handleMouseMove);
+            cancelAnimationFrame(animationFrameId);
         };
     }, [theme]);
 
--- src/data/constants.js
+++ src/data/constants.js
@@ -20,56 +20,56 @@ export const skills = [
       {
         name: "Angular",
         image:
-          "https://camo.githubusercontent.com/8886130b3d8aba95dbdd7c4f9a41029606424cc06d1873c1ced87dd55a222fef/68747470733a2f2f616e67756c61722e696f2f6173736574732f696d616765732f6c6f676f732f616e67756c61722f616e67756c61722e737667",
+          "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Angular-Dark.svg",
       },
       {
         name: "React Js",
         image:
-          "https://camo.githubusercontent.com/ff077b866cdc3fc2b0fa50ca6f8fa395451ffa625bc9c1133643b40e8afa2e2c/68747470733a2f2f736b696c6c69636f6e732e6465762f69636f6e733f693d7265616374",
+          "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/React-Dark.svg",
       },
       {
         name: "Vue Js",
         image:
-            "https://camo.githubusercontent.com/fca59a5a96d304d533795c7a6b78ad67a6ea3f43a0be9759f73980ec50d2cf41/68747470733a2f2f736b696c6c69636f6e732e6465762f69636f6e733f693d767565",
+            "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/VueJS-Dark.svg",
       },
       {
         name: "Nuxt Js",
         image:
-            "https://camo.githubusercontent.com/307e6e7b2379792d2b380257583304154ac84c80ef9a20e5c33acf4c7f2b240b/68747470733a2f2f736b696c6c69636f6e732e6465762f69636f6e733f693d6e757874",
+            "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/NuxtJS-Dark.svg",
       },
       {
         name: "Next Js",
         image:
-          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+          "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/NextJS-Dark.svg",
       },
       {
         name: "JavaScript",
         image:
-          "https://camo.githubusercontent.com/fe04e9b7078574e4a68b3bd9e97cbac84ba976a6efbdb80c9ff6068f5585f350/68747470733a2f2f736b696c6c69636f6e732e6465762f69636f6e733f693d6a617661736372697074",
+          "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/JavaScript.svg",
       },
       {
         name: "TypeScript",
         image:
-          "https://camo.githubusercontent.com/f470383a3e5e2cdf4e3872e62cb5a6f3c78de0d8deb5e5c5a9835976560e476f/68747470733a2f2f736b696c6c69636f6e732e6465762f69636f6e733f693d74797065736372697074",
+          "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/TypeScript.svg",
       },
       {
         name: "HTML",
-        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        image: "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/HTML.svg",
       },
       {
         name: "CSS",
         image:
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+          "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/CSS.svg",
       },
       {
         name: "Bootstrap",
         image:
-          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+          "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Bootstrap.svg",
       },
       {
         name: "Tailwind",
         image:
-          "https://camo.githubusercontent.com/0568e2de313626b2bd9b96f326941b012d45e9a4db1a23aa78bd8036207e57f8/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f7461696c77696e646373732f7461696c77696e646373732d69636f6e2e737667",
+          "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/TailwindCSS-Dark.svg",
       },
      
     ],
@@ -80,22 +80,22 @@ export const skills = [
       {
         name: "C#",
         image:
-          "https://camo.githubusercontent.com/70e5616dbd07222e33c75aeef97ac25e7ef9c5d57d5763c9a15f2fdc146418fd/68747470733a2f2f736b696c6c69636f6e732e6465762f69636f6e733f693d6373",
+          "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/CS.svg",
       },
       {
         name: "Java",
         image:
-          "https://camo.githubusercontent.com/70382ec8b6ad4f51d96246f0371f9660d4709cd5894734ab3de46dae24ef48e0/68747470733a2f2f736b696c6c69636f6e732e6465762f69636f6e733f693d6a617661",
+          "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Java-Dark.svg",
       },
       {
         name: "Python",
         image:
-          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+          "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Python-Dark.svg",
       },
       {
         name: "Go",
         image:
-          "https://camo.githubusercontent.com/17f96f9e3ac36030f80bd78f28d946dd9f047803dd5a9cfcf18d2628728b97d0/68747470733a2f2f736b696c6c69636f6e732e6465762f69636f6e733f693d676f",
+          "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/GoLang.svg",
       },
     ],
   },
@@ -110,20 +110,20 @@ export const skills = [
       {
         name: "MySQL",
         image:
-          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+          "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/MySQL-Dark.svg",
       },
       {
         name: "Postgresql",
-        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+        image: "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/PostgreSQL-Dark.svg",
       },
       {
         name: "MongoDB",
         image:
-          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+          "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/MongoDB.svg",
       },
       {
         name: "Firebase",
-        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+        image: "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Firebase-Dark.svg",
       },
     ]
   },
